d09722656e3381323843e2e5e99f42a2fa682c20
Target52 <gulien@me.com>
2021-09-11 14:29:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const age = 17;
+const isFemale = true;
+const driveStatus = 'bob';
+
+if (age >= 18) {
+    console.log("U mag naar binnen");
+} else {
+    console.log("U mag NIET naar binnen, te jong");
+}
+
+if (isFemale) {
+    console.log("U mag naar binnen bij ladiesnight");
+} else {
+    console.log("U mag NIET naar binnen bij ladiesnight");
+}
+
+if (driveStatus == 'bob') {
+    console.log("U mag rijden");
+} else {
+    console.log("U mag NIET rijden");
+}
+
+
